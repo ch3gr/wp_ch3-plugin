e3dea37fd2da20df7178b267f562959a39c2c423
Georgios Cherouvim <georgios.cherouvim@gmail.com>
2019-03-03 20:59:58 -0500

    gallery presets
    started on "images" and "posts" preset
    needs work

--- ch3-plugin.php
+++ ch3-plugin.php
@@ -385,12 +385,6 @@ add_action('print_media_templates', function(){
         <option value="posts"> Post Collection </option>
         <option value="custom"> Custom Options </option>
       </select>
-    </label>
-        <label class="setting">
-        <span><?php _e('Textarea'); ?></span>
-        <textarea value="test" data-setting="ds_textarea" id="testField" style="float:left;"></textarea>
-    </label>
-
 
     <h3 style="z-index: -1;">___________________________________________________________________________________________</h3>
 
@@ -402,7 +396,7 @@ add_action('print_media_templates', function(){
     $(function()
     {
         _.extend(wp.media.gallery.defaults, {
-        preset: 'custom',
+        preset: 'images',
         });
 
         wp.media.view.Settings.Gallery = wp.media.view.Settings.Gallery.extend({
@@ -411,18 +405,18 @@ add_action('print_media_templates', function(){
                    + wp.media.template('gallery-settings')(view);
             }
         });
-        $('#selectPreset option[value="images"]').attr("selected", "selected");
-        $('.testField option[value="images"]');
+        // $('#selectPreset option[value="images"]').attr("selected", "selected");
+        // $('.testField option[value="images"]');
 
-        $('#selectPreset').on('change', function() {
-            alert("aaa");
-            if ($(this).val() == 'custom') {
-                $('.link-to').prop('disabled', false);
-            } else {
-                // $('#selectTesty').reset();
-                $('.link-to').prop('disabled', true);
-            }
-        });
+        // $('#selectPreset').on('change', function() {
+        //     alert("aaa");
+        //     if ($(this).val() == 'custom') {
+        //         $('.link-to').prop('disabled', false);
+        //     } else {
+        //         // $('#selectTesty').reset();
+        //         $('.link-to').prop('disabled', true);
+        //     }
+        // });
     });
 
 
@@ -449,3 +443,229 @@ add_action('print_media_templates', function(){
 <?php
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+add_filter( 'post_gallery', 'my_post_gallery', 10, 2 );
+function my_post_gallery( $output, $attr) {
+    global $post, $wp_locale;
+
+    static $instance = 0;
+    $instance++;
+
+    // We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+    if ( isset( $attr['orderby'] ) ) {
+        $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+        if ( !$attr['orderby'] )
+            unset( $attr['orderby'] );
+    }
+
+    extract(shortcode_atts(array(
+        'preset'     => '',
+        'order'      => 'ASC',
+        'orderby'    => 'menu_order ID',
+        'id'         => $post->ID,
+        'itemtag'    => 'dl',
+        'icontag'    => 'dt',
+        'captiontag' => 'dd',
+        'columns'    => 3,
+        'size'       => 'thumbnail',
+        'include'    => '',
+        'exclude'    => ''
+    ), $attr));
+
+    $id = intval($id);
+    $output = '';
+
+
+
+
+
+    if( $preset == 'images' || $preset == ''){
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // Image Gallery
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        $output .= "Image preset<br>";
+
+
+        $selector = "gallery-{$instance}";
+        $output = apply_filters('gallery_style', "
+            <style type='text/css'>
+                #{$selector} {
+                    margin: auto;
+                }
+                #{$selector} img {
+                    border: 1px solid red;
+                }
+                
+            </style>
+            <!-- see gallery_shortcode() in wp-includes/media.php -->
+            <div id='$selector' class='gallery galleryid-{$id}'>");
+
+
+        $ids = explode(",", $include);
+        foreach( $ids as $id ) {
+            // $output .= '<div id=""'
+            $output .= wp_get_attachment_image( $id, 'large', 0, '' );
+            
+            // $output .=
+        }
+
+        $output .= '</div>';
+        $output .= '<br>END of gallery<br>__________<br>';
+        return $output;
+
+    }
+
+
+
+
+
+
+    else if( $preset == 'posts'){
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // Post listing
+        // The Image is a link to parent post
+        //////////////////////////////////////////////////////////////////////////////////////////
+        $output .= "Image preset<br>";
+
+        $selector = "gallery-{$instance}";
+        $output = apply_filters('gallery_style', "
+            <style type='text/css'>
+                #{$selector} {
+                    margin: auto;
+                }
+                #{$selector} img {
+                    border: 1px solid red;
+                }
+                
+            </style>
+            <!-- see gallery_shortcode() in wp-includes/media.php -->
+            <div id='$selector' class='gallery galleryid-{$id}'>");
+
+
+        $ids = explode(",", $include);
+        foreach( $ids as $id ) {
+            $output .= '<a href="'. get_permalink( wp_get_post_parent_id($id) ) .'">';
+            $output .= wp_get_attachment_image( $id, 'large', 0, '' );
+            $output .= '</a>';
+            
+        }
+
+        $output .= '</div>';
+        $output .= '<br>END of gallery<br>__________<br>';
+        return $output;
+    } 
+
+
+
+
+
+
+
+
+
+
+
+    else {
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // Custom Order
+
+        if ( 'RAND' == $order )
+            $orderby = 'none';
+
+        if ( !empty($include) ) {
+            $include = preg_replace( '/[^0-9,]+/', '', $include );
+            $_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+            $attachments = array();
+            foreach ( $_attachments as $key => $val ) {
+                $attachments[$val->ID] = $_attachments[$key];
+            }
+        } elseif ( !empty($exclude) ) {
+            $exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+            $attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+        } else {
+            $attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+        }
+
+        if ( empty($attachments) )
+            return '';
+
+        if ( is_feed() ) {
+            $output = "\n";
+            foreach ( $attachments as $att_id => $attachment )
+                $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+            return $output;
+        }
+
+        $itemtag = tag_escape($itemtag);
+        $captiontag = tag_escape($captiontag);
+        $columns = intval($columns);
+        $itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+        $float = is_rtl() ? 'right' : 'left';
+
+        $selector = "gallery-{$instance}";
+
+        $output = apply_filters('gallery_style', "
+            <style type='text/css'>
+                #{$selector} {
+                    margin: auto;
+                }
+                #{$selector} .gallery-item {
+                    float: {$float};
+                    margin-top: 10px;
+                    text-align: center;
+                    width: {$itemwidth}%;           }
+                #{$selector} img {
+                    border: 2px solid #cfcfcf;
+                }
+                #{$selector} .gallery-caption {
+                    margin-left: 0;
+                }
+            </style>
+            <!-- see gallery_shortcode() in wp-includes/media.php -->
+            <div id='$selector' class='gallery galleryid-{$id}'>");
+
+        $i = 0;
+        foreach ( $attachments as $id => $attachment ) {
+            $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+            $output .= "<{$itemtag} class='gallery-item'>";
+            $output .= "
+                <{$icontag} class='gallery-icon'>
+                    $link
+                </{$icontag}>";
+            if ( $captiontag && trim($attachment->post_excerpt) ) {
+                $output .= "
+                    <{$captiontag} class='gallery-caption'>
+                    " . wptexturize($attachment->post_excerpt) . "
+                    </{$captiontag}>";
+            }
+            $output .= "</{$itemtag}>";
+            if ( $columns > 0 && ++$i % $columns == 0 )
+                $output .= '<br style="clear: both" />';
+        }
+
+        $output .= "
+                <br style='clear: both;' />
+            </div>\n";
+        return $output;
+    } 
+
+
+    // $output .= '<br>END of post<br>__________<br>';
+
+
+}
